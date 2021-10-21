546a6be352c55f325e5e89eac34ba1a79b820ed4
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-21 18:40:35 +0530

    Update play.py

--- VCPlayBot/modules/play.py
+++ VCPlayBot/modules/play.py
@@ -498,7 +498,7 @@ async def play(_, message: Message):
         # lmoa = await client.get_chat_member(chid,wew)
     except:
         await lel.edit(
-            f"<i> {user.first_name} Userbot not in this chat, Ask admin to send /play command for first time or add {user.first_name} manually request here @AwesomeSupport with your group link</i>"
+            f"<i> {user.first_name} Userbot not in this chat, Ask admin to send /play command for first time or add {user.first_name} manually request here @SL_Tech_Worldchat with your group link</i>"
         )
         return
     text_links=None
