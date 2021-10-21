32092f9e6bb75d49bc1ed1c073558cca66ec4db6
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-21 19:21:17 +0530

    Update channelplay.py

--- VCPlayBot/modules/channelplay.py
+++ VCPlayBot/modules/channelplay.py
@@ -665,7 +665,7 @@ async def jiosaavn(client: Client, message_: Message):
                     # print(e)
                     await lel.edit(
                         f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your channel due to heavy requests for userbot! Make sure user is not banned in group."
-                        "\n\nOr manually add @VCPlayBot to your Group and try again</b>",
+                        "\n\nOr manually add @musiplayvc to your Group and try again</b>",
                     )
     try:
         await USER.get_chat(chid)
